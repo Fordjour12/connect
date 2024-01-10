a388bc7ffd7876f7937d9550bbbba108a9bc4b69
Fordjour12 <fordjourbobiem@gmail.com>
2024-01-10 00:19:39 +0000

    Remove Firebase configuration

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -12,13 +12,6 @@ import { getFirestore } from "firebase/firestore";
 // Initialize Firebase
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAa93TRKZtZQ7Mz4G49B5xXlkOxPrJ1vH8",
-  authDomain: "iocto-waffle.firebaseapp.com",
-  projectId: "iocto-waffle",
-  storageBucket: "iocto-waffle.appspot.com",
-  messagingSenderId: "254165628489",
-  appId: "1:254165628489:web:1b1f56ed68050aa3fd3198",
-  measurementId: "G-RE369GRV9J",
 };
 
 const app = initializeApp(firebaseConfig);
